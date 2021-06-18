825512b2ce37614080bcae2f3408d54ca8228cb6
Markus Ziermann <markus@fullmotion.de>
2021-06-18 10:12:54 +0200

    Added version

--- Casks/labcom.rb
+++ Casks/labcom.rb
@@ -1,11 +1,13 @@
 cask "labcom" do
-    version :latest
-    sha256 :no_check
+    version "1.3.38.0-macOS-BETA"
+    sha256 "869a7afc48975a88baa7e58370b64623cd51395f069b8709c459ad3043302af1"
 
     url "https://poollab.org/assets/website/download/software/Setup-MacOS.dmg"
     name "LabCom"
     desc "Pool water measurement manager"
     homepage "https://poollab.org/"
+
+    depends_on macos: ">= :el_capitan"
 
     app "LabCom.app"
 
